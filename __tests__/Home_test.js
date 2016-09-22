4d7da6bf0433d507546bd03b275d60dbb4285d99
Max Amey <maxwellamey@gmail.com>
2016-09-22 15:53:18 -0500

    added babelrc and moved babel dependencies to dev dependencies

---
+++ __tests__/Home_test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TestUtils from 'react-addons-test-utils'
+import Home from '../lib/Home'
+
+describe("Home", () => {
+  it("should pass", () => {
+    expect(true).toBe(true)
+  })
+})
